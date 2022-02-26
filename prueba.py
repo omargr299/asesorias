bc1bac55ca8314c3cccf4a7894c27878551cf61f
omargr299 <omar.gzz.rdz@hotmail.com>
2022-02-25 23:15:14 -0600

    1

---
+++ prueba.py
@@ -0,0 +1,113 @@
+'''
+print("Hola")
+
+print("Hola\"")
+print('\'')
+
+'secuencia de escape'
+print("hola\nMi\nNombre\nEs\nOmar\n\n")
+
+print("Hola\tComo estas")
+
+variable = 10
+print(variable)
+
+enteros = 10
+flotantes = 3.14
+string = "hola"
+string_2 = "8jkgvkjbvjvjlkl"
+boleano = True
+booleano_2 = False
+print(enteros)
+print(flotantes)
+print(string)
+print(boleano)
+print(booleano_2)
+
+print(type(enteros))
+print(type(flotantes))
+print(type(string))
+print(type(string_2))
+print(type(boleano))
+print(type(booleano_2))
+
+
+'suma'
+print(5+3)
+print(2.13+3.14)
+print(5.4+3)
+'print(3+"9")'
+print(True+True)
+print(3+True)
+
+'resta'
+print(4-2)
+
+'multiplicacion'
+print(6*3)
+print("hola"*4)
+
+'division'
+print(8/2)
+
+'exponensial'
+print(4**2)
+
+'modulo'
+print(8%2)
+print(5%2)
+
+'condicionales'
+==
+<
+>
+>=
+>=
+!=
+print(5==5)
+print(5==3)
+print(5>3)
+print(5<3)
+print(5<=5)
+print(5<=8)
+print(5!=8)
+print(8!=8)
+
+print("Hola" + " como estas")
+ 
+numero = 13
+print(numero)
+numero += 1
+print(numero)
+numero -= 1
+print(numero)
+numero *= 1
+print(numero)
+numero /= 2
+print(numero)
+numero %= 2
+print(numero)
+numero **= 2
+print(numero)
+
+numero = 2
+resultado = numero + 1
+print(numero, resultado)
+
+respuesta = input("Ingresa un dato:")
+print("El numero es:",respuesta)
+
+respuesta = input("Ingresa un dato:")
+otro_numero=11
+print(f"El numero es: {respuesta} y el otro numero es: {otro_numero}")
+
+int()
+float()
+str()
+n = int(input("Ingresa un numero: "))
+n = int("Hola")
+print(type(n))'''
+
+n = 1
+n_2= 2  
+print(f"El numero es: {n} y el otro es {n_2}")
